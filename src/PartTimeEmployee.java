926e9752336fc2b1c776f0c1a4b0ff9e13290b3a
ljn <ljn@10.29.194.149>
2016-12-13 06:53:32 -0500

    The file has been committed

--- src/PartTimeEmployee.java
+++ src/PartTimeEmployee.java
@@ -16,6 +16,7 @@ public class PartTimeEmployee extends EmployeeInfo // PartTimeEmployee is a
     private double hoursPerWeek;
     private double weeksPerYear;
 
+    //comstructors
     public PartTimeEmployee(int ID, String fName, String lName, int gender, int location, double dedRate, double wage,
             double hrsPerWeek, double weeks)
     {
@@ -25,22 +26,25 @@ public class PartTimeEmployee extends EmployeeInfo // PartTimeEmployee is a
         weeksPerYear = weeks;
     }
 
+    //getter method to get the hourly wage
     public double getHourlyWage()
     {
         return hourlyWage;
     }
 
+    //getter method to get the number of hours working per week
     public double getHoursPerWeek()
     {
         return hoursPerWeek;
     }
 
+    //getter method to get the number of weeks working per year
     public double getWeeksPerYear()
     {
         return weeksPerYear;
     }
 
-    
+    //to calculate annual income
     public double calcAnnualIncome()
     {
         return ((hourlyWage * hoursPerWeek * weeksPerYear) * (1 - deductionsRate));
